166a2f9c421f02f3975b9b1713c5efae00ddfd5f
Hope <hopeonasommerday@gmail.com>
2017-08-05 23:28:28 -0600

    Codewars-Katas
    Kata Solutions from Codewars in C#, JavaScript and PHPKata Solutions from Codewars in C#, JavaScript and PHP

---
+++ Codwars-Katas.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Codwars-Katas : MonoBehaviour {
+
+	//Kata Solutions from Codewars in C#, JavaScript and PHP
+}
