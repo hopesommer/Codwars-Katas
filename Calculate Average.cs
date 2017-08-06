f59410375c5fc13d54115c23b97762f6518e3950
Hope <hopeonasommerday@gmail.com>
2017-08-05 23:18:12 -0600

    Calculate Average
    Write function avg which calculates average of numbers in given list.

---
+++ Calculate Average.cs
@@ -0,0 +1,18 @@
+﻿//Write function avg which calculates average of numbers in given list.
+
+class AverageSolution
+{
+    public static double FindAverage(double[] array)
+    {
+        double total = 0;
+        double average = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+
+            total += array[i];
+            average = total / array.Length;
+        }
+        return average;
+    }
+
+}
