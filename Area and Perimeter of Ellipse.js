9cdcd050b3b212462441be3956bb040014cbf91c
Hope <hopeonasommerday@gmail.com>
2017-08-06 19:21:12 -0600

    Area and Perimeter of Ellipse
    Calculate area and perimeter of the elipse.
    You got dimensions of two axis "a" and "b" in centimeters.

---
+++ Area and Perimeter of Ellipse.js
@@ -0,0 +1,10 @@
+﻿/*
+Calculate area and perimeter of the elipse.
+
+You got dimensions of two axis "a" and "b" in centimeters.
+*/
+function elipse(a, b) {
+    var area = Math.PI * (a * b);
+    var perimeter = Math.PI * (3 / 2 * (a + b) - Math.sqrt(a * b));
+    return "Area: " + area.toFixed(1) + ", perimeter: " + perimeter.toFixed(1);
+}
